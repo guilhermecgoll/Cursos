4569de28166bf2d12abdd6fdb0985139a75c8730
Guilherme Goll <Guilherme Goll>
2017-03-07 15:52:19 -0300

    Ajuste no import do css

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.casadocodigo.loja.controllers.HomeController;
@@ -20,7 +22,7 @@ import br.com.casadocodigo.loja.infra.FileSaver;
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class, ProdutoDAO.class, FileSaver.class})
 //@ComponentScan(basePackages={"br.com.casadocodigo.loja.controllers"})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver(){
@@ -53,5 +55,11 @@ public class AppWebConfiguration {
 	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations(
+                "/resources/");
+    }
 
 }
